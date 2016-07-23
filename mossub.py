20d7faac45c268d1f60d0173700b25d5c7725de4
Stellar Zhang <muyuxingguang@163.com>
2016-07-23 14:40:46 +0800

    通天塔

---
+++ mossub.py
@@ -0,0 +1,21 @@
+import paho.mqtt.client as mqtt
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("hello")
+#订阅，第一个参数是订阅的主题
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect("115.28.93.126", 1883, 60)
+#第一个参数为主机名，及Mosquitto所在服务器，第二个参数是端口
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop_forever()
